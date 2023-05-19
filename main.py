abaf21c675f752a3dc80867f8f7f7e7c34fb88d6
MartinKotac <mkotevski65@gmail.com>
2023-05-19 17:15:58 +0200

    Adding cors origins

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Json
 import json
 
 from pipelineMedicineRDF import pipelineRDF
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class InputData(BaseModel):
